0be5f426e22cb62bd22a7951e4aa63afb7310b8a
Vladimir Matos <vladimir.matos@outlook.com>
2021-07-24 16:31:59 -0300

    feat (producer x consumer): add producer and consumer
    add producer simulator
    add consumer with event
    events send with load balancer of consumers

---
+++ Source/Consumer/Consumer.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Consumer
+{
+	class Consumer
+	{
+		static void Main()
+		{
+			Console.WriteLine("Start Message Consumer Instance!");
+
+			Initialize();
+		}
+
+		private static void Initialize()
+		{
+			var host = new ConnectionFactory()
+			{
+				HostName = "127.0.0.1",
+				UserName = "guest",
+				Password = "guest"
+			};
+
+			using var connection = host.CreateConnection();
+
+			using var channel = connection.CreateModel();
+			channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+			var eventingConsumer = new EventingBasicConsumer(channel);
+
+			Console.WriteLine($"Instace: {Guid.NewGuid()}");
+			Console.WriteLine("Waiting for messages...");
+
+			eventingConsumer.Received += (model, content) =>
+			{
+				var interval = new Random().Next(100, 500);
+				Thread.Sleep(interval);
+
+				Console.WriteLine($"Message: {Encoding.UTF8.GetString(content.Body.ToArray())} - Time: {interval}");
+
+				channel.BasicAck(deliveryTag: content.DeliveryTag, multiple: false);
+			};
+
+			channel.BasicConsume(queue: "Log", autoAck: false, consumer: eventingConsumer);
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Source/Producer/Producer.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Producer
+{
+	class Producer
+	{
+		static void Main()
+		{
+			Console.WriteLine("Start Message Producer!");
+
+			Initialize();
+		}
+
+		private static void Initialize()
+		{
+			var host = new ConnectionFactory()
+			{ 
+				HostName = "127.0.0.1",
+				UserName = "guest",
+				Password = "guest"
+			};
+
+			using var connection = host.CreateConnection();
+			using var channel = connection.CreateModel();
+
+			while(true)
+			{
+				var message = $"Operation performed id: {Guid.NewGuid()}";
+				
+				var properties = channel.CreateBasicProperties();
+				properties.Persistent = true;
+
+				Thread.Sleep(200);
+
+				channel.BasicPublish(exchange: string.Empty, routingKey: "Log", basicProperties: properties, body: Encoding.UTF8.GetBytes(message));
+
+				Console.WriteLine($"{DateTime.UtcNow} - Send: {message}");
+			}
+		}
+	}
+}
