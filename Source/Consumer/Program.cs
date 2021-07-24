57ebae386e504256fe6b676097dc0f298b65a589
Vladimir Matos <vladimir.matos@outlook.com>
2021-07-24 13:57:43 -0300

    build (initial): create initial structure
    add projects and default references
    add diagram of architecture

---
+++ Source/Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Consumer
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ Source/Producer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Producer
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
